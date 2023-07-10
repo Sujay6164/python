19c354c05ff0448f2e3b6ffb481f592f998bc843
Sujay6164 <139144906+Sujay6164@users.noreply.github.com>
2023-07-10 20:06:04 +0530

    Add files via upload

---
+++ tuples and its operations a.py
@@ -0,0 +1,6 @@
+def tuple_to_string(tuple):
+    string= ''.join(tuple)
+    return string
+tuple= ['P','Y','T','H','O','N']
+string= tuple_to_string(tuple)
+print(string)
---
+++ tuples and its operations b.py
@@ -0,0 +1,5 @@
+def reverse_tuple(t):
+    return tuple(reversed(t))
+t=('a','b','c','d','e','f')
+reversed_tuple = reverse_tuple(t)
+print(reversed_tuple)
